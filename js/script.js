8cfc57b52dafbb2e924a1b12e86490033091034d
ubuntujunior <yeohchongyi2@outlook.com>
2022-09-19 16:26:29 +0800

    Add function operate that operates on 2 numbers with input operator

--- js/script.js
+++ js/script.js
@@ -17,3 +17,18 @@ function divide(value1, value2) {
     //return value1 / value2
     return value1 / value2;
 }
+
+function operate(operator, value1, value2) {
+    // return value1 (operator) value2
+    //ex if operator = +, return value1 + value2
+    switch (true) {
+        case (operator.trim() === "+"):
+            return add(value1, value2);
+        case (operator.trim() === "-"):
+            return subtract(value1, value2);
+        case (operator.trim() === "*"):
+            return multiply(value1, value2);
+        case (operator.trim() === "/"):
+            return divide(value1, value2);
+    }
+}
