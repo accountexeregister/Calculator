cbf67e4917087a91cca1c54852ea2f6e62b78abc
ubuntujunior <yeohchongyi2@outlook.com>
2022-09-19 16:43:54 +0800

    Create basic HTML calculator with buttons without functions

--- js/script.js
+++ js/script.js
@@ -32,3 +32,48 @@ function operate(operator, value1, value2) {
             return divide(value1, value2);
     }
 }
+
+function generateInputButtons() {
+    //use loop to generate button from 0 to 9 digit
+    for (let i = 0; i < 10; i++) {
+        const button = document.createElement("button");
+        //assign button text content from 0 to 0 digit
+        button.textContent = i;
+        //append button to "input-container" div
+        document.querySelector(".input-container").appendChild(button);
+    }
+    //generate button for each function defined
+    //assign button text content to functions defined
+    //append buttons to "input-container" div
+
+    const addButton = document.createElement("button");
+    const subtractButton = document.createElement("button");
+    const multiplyButton = document.createElement("button");
+    const divideButton = document.createElement("button");
+    const operateButton = document.createElement("button");
+
+    addButton.textContent = "add";
+    subtractButton.textContent = "subtract";
+    multiplyButton.textContent = "multiply";
+    divideButton.textContent = "divide";
+    operateButton.textContent = "operate";
+
+    const inputContainer = document.querySelector(".input-container");
+    inputContainer.appendChild(addButton);
+    inputContainer.appendChild(subtractButton);
+    inputContainer.appendChild(multiplyButton);
+    inputContainer.appendChild(divideButton);
+    inputContainer.appendChild(operateButton);
+}
+
+function addClearButton() {
+    //create clear button and store it in variable "clearButton"
+    const clearButton = document.createElement("button");
+    //set text content of clearButton to "clear"
+    clearButton.textContent = "clear";
+    //append clearButton to "input-container" div
+    document.querySelector(".input-container").appendChild(clearButton);
+}
+
+generateInputButtons();
+addClearButton();
